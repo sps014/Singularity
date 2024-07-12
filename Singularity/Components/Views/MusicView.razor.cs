67a64dbabf6e1645cfd7d6c445a66891aaa833fa
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2024-07-12 22:11:09 +0530

    addng muic view

---
+++ Singularity/Components/Views/MusicView.razor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Maui.Core.Primitives;
+using Microsoft.AspNetCore.Components;
+using Singularity.Services;
+
+namespace Singularity.Components.Views;
+
+public  partial class MusicView
+{
+#nullable disable
+    [Inject]
+    public AudioManager AudioManager { get; set; }
+
+#nullable restore
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        AudioManager.MediaPlayer.StateChanged += MediaPlayerStateChanged;
+    }
+
+    private async void MediaPlayerStateChanged(object? sender, MediaStateChangedEventArgs e)
+    {
+        await InvokeAsync(() =>
+        {
+            StateHasChanged();
+
+        });
+    }
+}
