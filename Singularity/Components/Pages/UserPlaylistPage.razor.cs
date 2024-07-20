05eb52f9ed4e7748af93145b61d3191b7baddaae
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2024-07-21 00:00:43 +0530

    nav bug fixes

--- Singularity/Components/Pages/UserPlaylistPage.razor.cs
+++ Singularity/Components/Pages/UserPlaylistPage.razor.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Singularity.Components.Views;
 
 namespace Singularity.Components.Pages;
 
 public partial class UserPlaylistPage
 {
     private bool addPlaylistNewVisible = false;
-
+    private CreateNewPlaylistView? NewPlaylistElement;
     private async Task OnPlusButtonClicked()
     {
-        addPlaylistNewVisible=true;
+        NewPlaylistElement.SetOpen();
         StateHasChanged();
     }
 }
