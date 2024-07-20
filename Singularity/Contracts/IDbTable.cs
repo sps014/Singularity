e5df7edbd1a81ef635b8ffba387ad4ea33e764ed
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2024-07-20 10:27:34 +0530

    feat: adding datatable abstrations

---
+++ Singularity/Contracts/IDbTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singularity.Contracts;
+
+public interface IDbTable
+{
+    ValueTask SetValue<T>(string columnName, T value);
+    ValueTask<T> GetValue<T>(string columnName);
+    ValueTask<bool> DeleteValue(string columnName);
+    ValueTask<T> ToAsync<T>();
+}
