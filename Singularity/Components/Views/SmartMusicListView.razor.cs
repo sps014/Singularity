c80f536d59ddfb2de8d87395684269ec6d9d8bbc
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2024-07-20 20:25:17 +0530

    feat added vitualuzation

--- Singularity/Components/Views/SmartMusicListView.razor.cs
+++ Singularity/Components/Views/SmartMusicListView.razor.cs
@@ -3,29 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlazorBindGen;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using Singularity.Contracts;
 
 namespace Singularity.Components.Views;
 
-public partial class SmartMusicListView : ComponentBase
+public partial class SmartMusicListView : ComponentBase, IAsyncDisposable
 {
     [Parameter]
     public IAsyncEnumerable<ISong>? Songs { get; set; }
 
     [Parameter]
-    public int ViewItemCount { get; set; } = 15;
+    public int ViewItemCount { get; set; } = 10;
 
     public List<ISong> ResizableSongsList = new List<ISong>();
+
+    private ElementReference endElement;
+
+    private DotNetObjectReference<SmartMusicListView> dotnetObjectReference;
+
+    private bool isFinishedLoading = false;
 
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
+        dotnetObjectReference = DotNetObjectReference.Create(this);
 
         if (Songs == null)
             return;
 
         await AddNextSongBatch();
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (!firstRender)
+            return;
+
+        await BindGen.Window.CallVoidAsync("subscribeObserver", endElement,dotnetObjectReference);
     }
 
     private async ValueTask AddNextSongBatch()
@@ -36,17 +55,29 @@ public partial class SmartMusicListView : ComponentBase
         int start = ResizableSongsList.Count;
         int end = ResizableSongsList.Count+ViewItemCount;
 
-
+        int ct = 0;
         await foreach (var song in Songs)
         {
             if (start > end)
                 break;
 
             start++;
+            ct++;
             ResizableSongsList.Add(song);
         }
-
+        isFinishedLoading = ct == 0;
         StateHasChanged();
     }
 
+    public async ValueTask DisposeAsync()
+    {
+        await BindGen.Window.CallVoidAsync("unsubscribeObserver");
+        dotnetObjectReference?.Dispose();
+    }
+
+    [JSInvokable("visibiltyChanged")]
+    public async void VisibiltyChanged(bool visible)
+    {
+        await AddNextSongBatch();
+    }
 }
--- Singularity/wwwroot/js/app.js
+++ Singularity/wwwroot/js/app.js
@@ -1,1 +1,22 @@
-﻿
+﻿let observer;
+window.subscribeObserver = function (el, dotnet) {
+    observer = new IntersectionObserver(callback.bind(dotnet), {
+        root: document.getElementById("tabViewBody"),
+        rootMargin: "0px",
+        threshold: 0.01,
+    });
+
+    observer.observe(el);
+}
+window.unsubscribeObserver = function()
+{
+    if (observer != undefined)
+        observer.disconnect();
+}
+function callback(e) {
+    for (let entry of e) {
+        const visible = entry.intersectionRatio != 0;
+        this.invokeMethodAsync("visibiltyChanged", visible);
+        //console.log(this.i+"  -> "+entry.intersectionRatio);
+    }
+}
